bb8705bcd27c22250d62e485a854944d9aa3314d
Gisleudo Cortez <gisleudo.cmf@gmail.com>
2026-02-02 15:50:09 -0300

    initial command for initializing active

--- cli/active_cli.py
+++ cli/active_cli.py
@@ -1,13 +1,22 @@
 import argparse
 
+from lib.active_class import Active
+
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Active CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
-    args = parser.parse_args()
+    active_parser = subparsers.add_parser(
+        name="active", help="Initialize a active for monitoring"
+    )
+    active_parser.add_argument("active", help="active to be monitored")
 
+    args = parser.parse_args()
     match args.command:
+        case "active":
+            active = Active(args.active)
+            print()
         case _:
             parser.print_help()
 
